1bd1ebd81e8adc77ca2a190bdbea425fe3c99a39
Erik Rodriguez <eric19.21.03@gmail.com>
2020-01-30 21:32:49 -0800

    Add form using formik dependency
    App.js renders form and has 4 inputs with a submit button

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import TheForm from "./TheForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Onboard System:</h1>
+      <TheForm />
+    </div>
+  );
 }
 
 export default App;
